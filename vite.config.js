707fbdce507df58d1800edac1af97595b2043b8b
samuel <samuelhermany@hotmail.com>
2024-09-30 02:04:49 -0300

    ajustado erro de caminho

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// npm run deploy
 export default defineConfig({
   plugins: [react()],
   base: "/PB_MyFilmeFlix",
